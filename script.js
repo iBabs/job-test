716fa2642cffd50189be78db92467d121027788b
iBabs <ibrahimbabalola8@gmail.com>
2023-05-09 00:09:51 +0100

    final commit

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ window.addEventListener('scroll', () => {
 const say = () =>{
     alert('welcome')
 }
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el)=> observer.observe(el))
